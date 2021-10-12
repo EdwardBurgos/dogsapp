4d59023cd3898f6bc0a293799f23eaa073b182a2
EdwardBurgos <edwardpbn@gmail.com>
2021-10-12 08:26:19 -0500

    improved community user styles

--- client/src/components/CommunityMember/CommunityMember.jsx
+++ client/src/components/CommunityMember/CommunityMember.jsx
@@ -7,16 +7,20 @@ export default function CommunityMember({ fullname, profilepic, username, countr
   return (
     <Link to={`/${username}`} className={`${s.card} linkRR`}>
       <div className={s.infoContainer}>
-        <div className={s.flagContainer}>
-          <img className={s.flag} src={flag} alt={`${country} flag`} />
+        <div className={s.imageContainer}>
+          <img className={s.image} src={profilepic} alt={username} />
         </div>
         <div className={s.dataContainer}>
           <p className='mb-0'>{fullname}</p>
           <p className='mb-0'>{username}</p>
-          <p className='mb-0'>{country}</p>
+          <div className={s.countryInfo}>
+            <img className={s.flag} src={flag} alt={`${country} flag`} />
+            <p className='mb-0'>{country}</p>
+          </div>
+
         </div>
       </div>
-      <img className={s.image} src={profilepic} alt={username} />
+
     </Link>
   );
 }
